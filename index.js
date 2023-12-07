465513c30f3228a2eb1c967028e13e66e3a2fd6e
DMaladyn <damian.maladyn@gmail.com>
2023-12-07 13:21:58 +0100

    Passing all tests with comments

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+//multeris package that allows node to receive files from forms and analyzes them
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+//returns analysis of uploaded file that is inside req.file.[property]
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
 
-
+  //path where file is stored
+  //__dirname + "/" + req.file.path
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
